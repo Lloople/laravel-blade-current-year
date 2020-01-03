85ce6244a0c76618d32a2794c2803a0be8390afa
David Llop <d.lloople@icloud.com>
2020-01-03 19:26:14 +0100

    Test directive in blade view

--- tests/DirectiveTest.php
+++ tests/DirectiveTest.php
@@ -2,6 +2,8 @@
 
 namespace Lloople\LaravelBladeCurrentYear\Tests;
 
+use Illuminate\Support\Facades\View;
+
 class DirectiveTest extends TestCase
 {
     /** @test */
@@ -11,5 +13,13 @@ class DirectiveTest extends TestCase
             "<?php echo date('Y'); ?>", 
             app('blade.compiler')->compileString('@currentYear')
         );
+    }
+
+    /** @test */
+    public function it_renders_the_current_year()
+    {
+        View::addLocation(__DIR__.'/stubs/views');
+
+        $this->assertEquals(date('Y'), view('directive')->render());
     }
 }
